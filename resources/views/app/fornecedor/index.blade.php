cca235a3acf247d9b26b3f82c2c0f40aadb68f5a
lndsilva <lndsilva@hotmail.com>
2024-12-09 10:30:36 -0300

    sintaxe blade

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,13 @@
+<h3>Fornecedor</h3>
+{{-- Comentário que será descartado pelo blade --}}
+{{ 'Texto de teste'}}
+<?= 'Texto de texte' ?>
+
+@php
+//comentário de linha
+/*
+comentário de bloco
+*/
+echo '<br> Teste dentro do php no blade';
+
+@endphp
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ Route::prefix('/app')->group(function () {
     Route::get('/clientes', function () {
         return 'Clientes';
     })->name('app.clientes');
-    Route::get('/fornecedores', function () {
-        return 'Fornecedores';
-    })->name('app.fornecedores');
+    Route::get('/fornecedores',[\App\Http\Controllers\FornecedorController::class, 'index'])->name('app.fornecedor');
     Route::get('/produtos', function () {
         return 'Produtos';
     })->name('app.produtos');
