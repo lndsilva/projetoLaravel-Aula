ae8e59fa1b38d8597d4408851e2f7c5f32849931
lndsilva <lndsilva@hotmail.com>
2024-12-09 10:43:56 -0300

    sintaxe blade @if/@elseif

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,13 +1,14 @@
 <h3>Fornecedor</h3>
-{{-- Comentário que será descartado pelo blade --}}
-{{ 'Texto de teste'}}
-<?= 'Texto de texte' ?>
 
 @php
-//comentário de linha
-/*
-comentário de bloco
-*/
-echo '<br> Teste dentro do php no blade';
+
 
 @endphp
+
+@if (count($fornecedores) > 0 && count($fornecedores)< 10)
+    <h3>Existem alguns fornecedores cadastrados</h3>
+@elseif(count($fornecedores) > 10)
+    <h3>Existem vários fornecedores cadastrados</h3>    
+@else
+    <h3>Não existem fornecedores cadastrados</h3>
+@endif
